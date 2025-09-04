0222b9dd5bd4618b5226a7910267b7037789043c
Stani <stasja2000@yahoo.de>
2025-09-04 16:56:24 +0200

    stuff from the lab

--- experiments/recreation-2.js
+++ experiments/recreation-2.js
@@ -5,13 +5,22 @@ function setup() {
   background(200);
 }
 
+function element(x, y) {
+  let b = round(random(1, 3));
+  if (random(0, 10) <= 5) {
+    rect(x, y, 5, b * 5);
+  } else {
+    rect(x, y, b * 5, 5);
+  }
+}
+
 function row(number) {
-  let width = round(random(1, 2));
-  let height = round(random(1, 4));
-  let x = random(number * -5, number * 5);
+  //let width = round(random(1, 2));
+  //let height = round(random(1, 4));
+  let a = round(random(number * -5, number * 5));
 
   for (let i = 1; i <= number * 2; i++) {
-    rect(x, rowCounter * 5, width * 5, height * 5);
+    element(a, rowCounter * 5);
   }
 
   rowCounter += 1;
@@ -26,12 +35,12 @@ function draw() {
     numberRows -= 1;
   }
   for (let j = 2; j <= numberRows / 2; j += 2) {
-    row(j);
     //fill(random(0, 255), random(0, 255), random(0, 255));
+    row(j);
   }
   for (let k = numberRows / 2; k >= 2; k -= 2) {
-    row(k);
     //fill(random(0, 255), random(0, 255), random(0, 255));
+    row(k);
   }
   noLoop();
 }
